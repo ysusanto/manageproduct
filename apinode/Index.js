efb5084f8374356fd26425d4b0e4cc88133420e5
ari <yohanessusanto@seatech.com>
2021-10-06 17:46:54 +0700

    init2

--- apinode/Index.js
+++ apinode/Index.js
@@ -12,11 +12,6 @@ app.use(express.urlencoded({
 
 // parse requests of content-type: application/x-www-form-urlencoded
 
-app.get('/helloworld', (req, res) => {
-    res.json({
-        message: "Welcome to bezkoder application."
-    });
-});
 require("./app/routes/user.routes.js")(app);
 app.listen(port, () => {
     console.log(`cli-nodejs-api listening at http://localhost:${port}`)
